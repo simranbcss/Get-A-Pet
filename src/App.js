e6332ca0aa6f63598e382574a29ae88c78e01c4d
Sheikh Abdul Wahid <sheikhabdul.bcss19@iba-suk.edu.pk>
2022-02-27 03:05:53 +0500

    Adding Some front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header'
+import Products from './components/Products'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {DataProvider} from './components/DataProvider'
+import Details from './components/Details'
+import Cart from './components/Cart'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataProvider>
+      <div className="App">
+        <Router>
+          <Header />
+
+          <section>
+            <Routes>
+              <Route path="products" element={ <Products /> } />
+              <Route path="products/:id" element={ <Details /> } />
+              <Route path="cart" element={ <Cart /> } />
+            </Routes>
+          </section>
+        </Router>
+      </div>
+    </DataProvider>
   );
 }
 
