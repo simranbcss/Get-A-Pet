1fd3689b313b8f1be66d89a56d1aa3ab2e994392
simranbcss <62022022+simranbcss@users.noreply.github.com>
2022-03-04 00:37:04 +0500

    Update dboperations.js
    Added simple method to get the data that also make sure the proper connectivity with database....

--- dboperations.js
+++ dboperations.js
@@ -3,3 +3,21 @@ var config=require('./dbconfig');
 const sql = require('mysql');
 //for creating sql connection
 const pool  = sql.createPool(config);
+
+//used Assyncronous because by default it works in synchronous manner
+async function getData(res, query) {
+    //created the connection
+    pool.getConnection((err, connection) => {
+        //After connecting with database designed the query
+        connection.query(query, (err, rows) => {
+            connection.release() // return the connection to pool
+
+            if (!err) {
+                res.status(200).json(rows);
+            } else {
+                res.status(400).json(err);
+            }
+            console.log('The data from  table are: \n', rows)
+        })
+    });
+  }
